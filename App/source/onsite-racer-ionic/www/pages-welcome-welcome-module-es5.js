ea4037260742117a0b529ff148c4816e3e9bd057
christian.pitters <christian.pitters@t-systems.com>
2020-11-30 17:20:57 +0100

    add CAD Files, add app source code, update implementation guide, add smaple solution

---
+++ App/source/onsite-racer-ionic/www/pages-welcome-welcome-module-es5.js
@@ -0,0 +1,120 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["pages-welcome-welcome-module"],{
+
+/***/ "./node_modules/raw-loader/index.js!./src/app/pages/welcome/welcome.page.html":
+/*!***************************************************************************!*\
+  !*** ./node_modules/raw-loader!./src/app/pages/welcome/welcome.page.html ***!
+  \***************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\r\n  <ion-toolbar color=\"primary\">\r\n      <img src=\"assets/iotracer/welcome_1.svg\" style=\"width: auto; height: auto\">\r\n  </ion-toolbar>\r\n</ion-header>\r\n\r\n<ion-content padding color=\"dark\">\r\n  <ion-grid>\r\n    <ion-row>\r\n      <ion-col>\r\n        <h1>Welcome to the Onsite Racer!</h1>\r\n      </ion-col>\r\n    </ion-row>\r\n    <ion-row>\r\n      <ion-col>\r\n        <h3>With the Onsite Racer App you can steer your Car by remote.</h3>\r\n      </ion-col>\r\n    </ion-row>\r\n    <ion-row>\r\n      <ion-col>\r\n        <ion-button expand=\"block\" fill=\"outline\" coler=\"primary\" shape=\"round\" (click)=\"navigateToSteuerung()\">Let's Drive</ion-button>\r\n      </ion-col>\r\n    </ion-row>\r\n    <ion-row>\r\n      <ion-col style=\"text-align: center\">\r\n        <br>\r\n        <h5>by</h5>\r\n        <img src=\"assets/iotracer/Logos/InnovationLAB-WEST.png\" width=\"40%\" style=\"text-align: center\">\r\n      </ion-col>\r\n    </ion-row>\r\n  </ion-grid>\r\n</ion-content>\r\n"
+
+/***/ }),
+
+/***/ "./src/app/pages/welcome/welcome.module.ts":
+/*!*************************************************!*\
+  !*** ./src/app/pages/welcome/welcome.module.ts ***!
+  \*************************************************/
+/*! exports provided: WelcomePageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "WelcomePageModule", function() { return WelcomePageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _welcome_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./welcome.page */ "./src/app/pages/welcome/welcome.page.ts");
+
+
+
+
+
+
+
+var routes = [
+    {
+        path: '',
+        component: _welcome_page__WEBPACK_IMPORTED_MODULE_6__["WelcomePage"]
+    }
+];
+var WelcomePageModule = /** @class */ (function () {
+    function WelcomePageModule() {
+    }
+    WelcomePageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_5__["IonicModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forChild(routes)
+            ],
+            declarations: [_welcome_page__WEBPACK_IMPORTED_MODULE_6__["WelcomePage"]]
+        })
+    ], WelcomePageModule);
+    return WelcomePageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/pages/welcome/welcome.page.scss":
+/*!*************************************************!*\
+  !*** ./src/app/pages/welcome/welcome.page.scss ***!
+  \*************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "h3 {\n  font-size: 16px;\n  text-align: center;\n}\n\nh1 {\n  text-align: center;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNyYy9hcHAvcGFnZXMvd2VsY29tZS9DOlxcVXNlcnNcXGNwaXR0ZXJzXFxERVZcXEdJVFxcaW90LXJhY2VyLWJpdGJ1Y2tldFxcb25zaXRlcmFjZXJcXE9uU2l0ZVJhY2VyX3YyXFxBcHBcXG9uc2l0ZS1yYWNlci1pb25pYy9zcmNcXGFwcFxccGFnZXNcXHdlbGNvbWVcXHdlbGNvbWUucGFnZS5zY3NzIiwic3JjL2FwcC9wYWdlcy93ZWxjb21lL3dlbGNvbWUucGFnZS5zY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0VBQ0ksZUFBQTtFQUNBLGtCQUFBO0FDQ0o7O0FEQ0E7RUFDSSxrQkFBQTtBQ0VKIiwiZmlsZSI6InNyYy9hcHAvcGFnZXMvd2VsY29tZS93ZWxjb21lLnBhZ2Uuc2NzcyIsInNvdXJjZXNDb250ZW50IjpbImgze1xyXG4gICAgZm9udC1zaXplOiAxNnB4O1xyXG4gICAgdGV4dC1hbGlnbjogY2VudGVyO1xyXG59XHJcbmgxe1xyXG4gICAgdGV4dC1hbGlnbjogY2VudGVyO1xyXG59IiwiaDMge1xuICBmb250LXNpemU6IDE2cHg7XG4gIHRleHQtYWxpZ246IGNlbnRlcjtcbn1cblxuaDEge1xuICB0ZXh0LWFsaWduOiBjZW50ZXI7XG59Il19 */"
+
+/***/ }),
+
+/***/ "./src/app/pages/welcome/welcome.page.ts":
+/*!***********************************************!*\
+  !*** ./src/app/pages/welcome/welcome.page.ts ***!
+  \***********************************************/
+/*! exports provided: WelcomePage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "WelcomePage", function() { return WelcomePage; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+
+
+
+var WelcomePage = /** @class */ (function () {
+    function WelcomePage(router) {
+        this.router = router;
+    }
+    WelcomePage.prototype.ngOnInit = function () {
+    };
+    WelcomePage.prototype.navigateToSteuerung = function () {
+        this.router.navigate(['home/steuerung']);
+    };
+    WelcomePage.ctorParameters = function () { return [
+        { type: _angular_router__WEBPACK_IMPORTED_MODULE_2__["Router"] }
+    ]; };
+    WelcomePage = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-welcome',
+            template: __webpack_require__(/*! raw-loader!./welcome.page.html */ "./node_modules/raw-loader/index.js!./src/app/pages/welcome/welcome.page.html"),
+            styles: [__webpack_require__(/*! ./welcome.page.scss */ "./src/app/pages/welcome/welcome.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_angular_router__WEBPACK_IMPORTED_MODULE_2__["Router"]])
+    ], WelcomePage);
+    return WelcomePage;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=pages-welcome-welcome-module-es5.js.map
--- README.md
+++ README.md
@@ -4,3 +4,46 @@
 Workshop Documentation for T-onsite Racer @ CMD+O
 
 ![alt text](images/T-Racer3.jpg)
+
+## Respository structure
+
+**root directory:** Building instruction and programming instructions to build T-onsite-Racer.
+
+**Groupwork**: Introdcution to Bluetooth Low Energy and Microcontroller.
+
+**CAD::** T-Racer CAD Files.
+
+**Arduino:** Template sketch as "programming guide" for the T-Racer and sample solution.
+
+**App:** Link and introdcution to download the T-Racer app directly from AppStore/PlayStore to controll your car. For Android there is an prebuild apk. You can also find the source code of the app in the subdirectory "sources".
+
+**images:** Pictures and impressions of the T-Racer.
+
+## App
+
+We have a prebuild IOS and Android app, which can be officially downloaded as a trial version in the respective stores. You can find the link to the app for your platform in the app subfolder.
+
+You can also find the source code here. So you can build your app for the corresponding platform. The app is implemented in ionic and can be built for both, android and ios. First the development/build environment has to be configured accordingly:
+
+1. Clone this repo
+2. Install Node.js current version (https://nodejs.org/en/download/)
+3. Install the Ionic CLI with npm in CMD - npm install -g @ionic/cli (https://ionicframework.com/docs/intro/cli)
+4. Download Android SDK and JDK8 (https://ionicframework.com/docs/developing/android)
+5. Download Gradle and set Path to System (https://gradle.org/install/)
+6. Use IDE for modifications: example (Visual Studio Code - https://code.visualstudio.com/)
+7. In project directory, install nippljs - npm i nipplejs (https://www.npmjs.com/package/nipplejs)
+
+How to deploy on android device:
+Note: Compatible with Android 5.2 and higher.
+
+1. In project directory, run - ionic cordova prepare android (https://ionicframework.com/docs/cli/commands/cordova-prepare)
+2.  In project directory, run - ionic cordova run android -1 (to deploy on android Phone: https://ionicframework.com/docs/cli/commands/cordova-run)
+
+Visit "https://ionicframework.com/docs" for complete documentation.
+
+## T-Racer "tuning"
+
+To make the T-Racer a bit faster, you can buy a new engine, for example with 900rpm. Make sure that the dimensions are correct, otherwise it won't fit into the provided holder.
+
+You can also turn up the step-up converter to 10-12V to get the maximum power out of your motor. 
+Attention: The servo motor has an operating voltage of 5V. According to the wiring diagram, the servo is connected to the output of the step-up. If the step-up is turned up to over 5V, the servo must be replugged. You can then connect the servo motor directly to the battery.
